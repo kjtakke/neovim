106f84d7e44654c4be4b148f2f72d9de2c843613
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-04-04 09:46:50 +1000

    Update ai_ui.lua

--- lua/ai_ui.lua
+++ lua/ai_ui.lua
@@ -4,20 +4,19 @@ local M = {}
 local question_buf, answer_buf
 local question_win, answer_win
 
--- Set your API endpoint here
-local API_ENDPOINT = "http://localhost:8000/ask"
+local API_ASK_ENDPOINT   = "http://localhost:5001/ask"
+local API_CLEAR_ENDPOINT = "http://localhost:5001/clear_assistant"
 
 function M.open_ui()
   local width = vim.o.columns
   local height = vim.o.lines
 
-  -- Calculate sizes
   local question_height = 8
   local answer_height = height - question_height - 6
   local win_width = math.floor(width * 0.8)
   local win_col = math.floor((width - win_width) / 2)
 
-  -- Question box
+  -- 1. Create the question buffer and window
   question_buf = vim.api.nvim_create_buf(false, true)
   question_win = vim.api.nvim_open_win(question_buf, true, {
     relative = "editor",
@@ -28,11 +27,19 @@ function M.open_ui()
     border = "rounded",
     style = "minimal",
   })
-  vim.api.nvim_buf_set_option(question_buf, 'filetype', 'markdown')
-  vim.api.nvim_buf_set_keymap(question_buf, 'n', '<CR>', '', { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(question_buf, 'n', '<leader>s', "<cmd>lua require'ai_ui'.submit_question()<CR>", { noremap = true, silent = true })
+  vim.api.nvim_buf_set_option(question_buf, "filetype", "markdown")
+  vim.api.nvim_buf_set_keymap(
+    question_buf, "n", "<leader>s",
+    "<cmd>lua require'ai_ui'.submit_question()<CR>",
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_buf_set_keymap(
+    question_buf, "n", "<leader>z",
+    "<cmd>lua require'ai_ui'.clear_chat()<CR>",
+    { noremap = true, silent = true }
+  )
 
-  -- Answer box
+  -- 2. Create the answer buffer and window
   answer_buf = vim.api.nvim_create_buf(false, true)
   answer_win = vim.api.nvim_open_win(answer_buf, false, {
     relative = "editor",
@@ -43,25 +50,56 @@ function M.open_ui()
     border = "rounded",
     style = "minimal",
   })
-  vim.api.nvim_buf_set_option(answer_buf, 'filetype', 'markdown')
-  vim.api.nvim_buf_set_option(answer_buf, 'modifiable', false)
+  vim.api.nvim_buf_set_option(answer_buf, "filetype", "markdown")
+  vim.api.nvim_buf_set_option(answer_buf, "modifiable", false)
+
+  -- 3. Clear the server chat history but do NOT show its response.
+  M.clear_chat(true)  -- pass a flag, meaning "no need to show server's response"
 end
 
 function M.submit_question()
   local lines = vim.api.nvim_buf_get_lines(question_buf, 0, -1, false)
   local question = table.concat(lines, "\n")
 
-  -- HTTP request via curl (synchronous for simplicity)
-  local curl_cmd = string.format([[curl -s -X POST -H "Content-Type: application/json" -d '{"question": %q}' "%s"]], question, API_ENDPOINT)
+  local curl_cmd = string.format(
+    [[curl -s -X POST -H "Content-Type: application/json" -d '{"question": %q}' "%s"]],
+    question,
+    API_ASK_ENDPOINT
+  )
   local result = vim.fn.systemlist(curl_cmd)
 
-  -- Update answer window
-  vim.api.nvim_buf_set_option(answer_buf, 'modifiable', true)
-  -- vim.api.nvim_buf_set_lines(answer_buf, 0, -1, false, result)
-  vim.api.nvim_buf_set_lines(answer_buf, 0, -1, false, {"hello world"})
-  vim.api.nvim_buf_set_option(answer_buf, 'modifiable', false)
+  vim.api.nvim_buf_set_option(answer_buf, "modifiable", true)
+  vim.api.nvim_buf_set_lines(answer_buf, 0, -1, false, result)
+  vim.api.nvim_buf_set_option(answer_buf, "modifiable", false)
+
+ local line_count = vim.api.nvim_buf_line_count(answer_buf)
+ vim.api.nvim_win_set_cursor(answer_win, { line_count, 0 })
+ vim.api.nvim_buf_set_lines(question_buf, 0, -1, false, {})
 end
 
+function M.clear_chat(no_show)
+  if not answer_buf or not question_buf then
+    return
+  end
 
+  local curl_cmd = string.format([[curl -s -X POST "%s"]], API_CLEAR_ENDPOINT)
+  local result = vim.fn.systemlist(curl_cmd)
+
+  -- Either show the server response or ignore it
+  vim.api.nvim_buf_set_option(answer_buf, "modifiable", true)
+
+  if no_show then
+    -- Just wipe the answer buffer so it's blank
+    vim.api.nvim_buf_set_lines(answer_buf, 0, -1, false, {})
+  else
+    -- If called from Leader+z, maybe we *do* want to see what the server responded
+    vim.api.nvim_buf_set_lines(answer_buf, 0, -1, false, result)
+  end
+
+  vim.api.nvim_buf_set_option(answer_buf, "modifiable", false)
+
+  -- Also clear the question buffer if you like
+  vim.api.nvim_buf_set_lines(question_buf, 0, -1, false, {})
+end
 
 return M
