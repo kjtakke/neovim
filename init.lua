e95a3467260cd68a25b3b99d45b8d4448c33d3d8
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-04-06 08:42:53 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -161,6 +161,5 @@ end, { noremap = true, silent = true, desc = "VSplit + New Scratch" })
 require("lazy").setup("plugins")
 
 -- AI UI
-vim.api.nvim_set_keymap('n', '<leader>ai', "<cmd>lua require'ai_ui'.open_ui()<CR>", { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<leader>ai', [[<cmd>lua require'ai_ui'.set_last_buf()<CR><cmd>lua require'ai_ui'.open_ui()<CR>]], { noremap = true, silent = true })
 
