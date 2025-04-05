fb4814f11640a9054bc69b0331c70643c464d7ce
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-04-06 08:41:55 +1000

    Update ai_ui.lua

--- lua/ai_ui.lua
+++ lua/ai_ui.lua
@@ -1,8 +1,8 @@
-
 local M = {}
 
 local question_buf, answer_buf
 local question_win, answer_win
+local last_buf
 
 local API_ASK_ENDPOINT   = "http://localhost:5001/ask"
 local API_CLEAR_ENDPOINT = "http://localhost:5001/clear_assistant"
@@ -10,7 +10,7 @@ local API_CLEAR_ENDPOINT = "http://localhost:5001/clear_assistant"
 function M.open_ui()
   local width = vim.o.columns
   local height = vim.o.lines
-
+  
   local question_height = 8
   local answer_height = height - question_height - 6
   local win_width = math.floor(width * 0.8)
@@ -39,6 +39,13 @@ function M.open_ui()
     { noremap = true, silent = true }
   )
 
+
+  vim.api.nvim_buf_set_keymap(
+  question_buf, 'n', '\\f',
+  "<cmd>lua require'ai_ui'.insert_current_file()<CR>",
+  { noremap = true, silent = true }
+)
+
   -- 2. Create the answer buffer and window
   answer_buf = vim.api.nvim_create_buf(false, true)
   answer_win = vim.api.nvim_open_win(answer_buf, false, {
@@ -56,6 +63,7 @@ function M.open_ui()
   -- 3. Clear the server chat history but do NOT show its response.
   M.clear_chat(true)  -- pass a flag, meaning "no need to show server's response"
 end
+
 
 function M.submit_question()
   local lines = vim.api.nvim_buf_get_lines(question_buf, 0, -1, false)
@@ -118,5 +126,54 @@ function M.clear_chat(no_show)
   -- Also clear the question buffer if you like
   vim.api.nvim_buf_set_lines(question_buf, 0, -1, false, {})
 end
+
+
+function M.set_last_buf()
+  last_buf = vim.api.nvim_get_current_buf()
+end
+
+-- Get previous window's buffer
+function M.get_previous_buffer()
+  local current_win = vim.api.nvim_get_current_win()
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+
+  for _, win in ipairs(wins) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if win ~= current_win and vim.api.nvim_buf_get_option(buf, 'buftype') == '' then
+      return buf
+    end
+  end
+
+  return nil
+end
+
+
+function M.insert_current_file()
+  if not last_buf or not vim.api.nvim_buf_is_valid(last_buf) then
+    print("No previous buffer found")
+    return
+  end
+
+  -- Get content from the last buffer
+  local total_lines = vim.api.nvim_buf_line_count(last_buf)
+  local all_lines = vim.api.nvim_buf_get_lines(last_buf, 0, -1, false)
+
+  local insert_lines
+  if total_lines > 1000 then
+    local top = vim.list_slice(all_lines, 0, 300)
+    local bottom = vim.list_slice(all_lines, total_lines - 700, total_lines)
+    insert_lines = {}
+    vim.list_extend(insert_lines, top)
+    table.insert(insert_lines, "... [truncated middle] ...")
+    vim.list_extend(insert_lines, bottom)
+  else
+    insert_lines = all_lines
+  end
+
+  -- Append to the bottom of the question buffer
+  local current_lines = vim.api.nvim_buf_line_count(question_buf)
+  vim.api.nvim_buf_set_lines(question_buf, current_lines, current_lines, false, insert_lines)
+end
+
 
 return M
