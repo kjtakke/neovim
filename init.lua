508d3f2afde5cd1e310e76ffa03c0321e236fc8d
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-04-28 08:41:43 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -248,3 +248,6 @@ vim.api.nvim_create_user_command('Wtsh', run_shellcheck, {})
 
 -- Set colorscheme
 vim.cmd.colorscheme "catppuccin"
+-- AI UI
+vim.api.nvim_set_keymap('n', '<leader>ai', [[<cmd>lua require'ai_ui'.set_last_buf()<CR><cmd>lua require'ai_ui'.open_ui()<CR>]], { noremap = true, silent = true })
+
