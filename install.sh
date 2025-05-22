a57ec8b130a196d5721fc098d5c36eb47a827dd0
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2025-05-22 14:49:41 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,6 +41,7 @@ sudo npm install -g bash-language-server
 # Clone configuration repository and set up NeoVim
 git clone https://github.com/kjtakke/neovim.git ~/nvim-clone
 cp -f ~/nvim-clone/init.lua ~/.config/nvim/init.lua
+cp -f ~/nvim-clone/search/nsearch.txt ~/.config/nvim/nsearch.txt
 cp -f ~/nvim-clone/lazy-lock.json ~/.config/nvim/lazy-lock.json
 cp -f ~/nvim-clone/lua/cmp.lua.bak ~/.config/nvim/lua/cmp.lua.bak
 cp -f ~/nvim-clone/lua/init.lua ~/.config/nvim/lua/init.lua
@@ -50,5 +51,7 @@ cp -f ~/nvim-clone/lua/plugins/init.lua ~/.config/nvim/lua/plugins/init.lua
 # Remove temporary clone directory
 rm -rf ~/nvim-clone
 
+source ~/.zshrc
+source ~/.bashrc
 # Launch NeoVim
 nvim
